688914869fe46236b956fb4ec3d1013b5323cc87
Asia Ahmad Ali <asiaahmadali17@gmail.com>
2024-11-14 21:39:12 +0500

    deleting items

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 console.log("testing application");
 console.log("adding new products");
 console.log("deleting items") ;
-console.log("updating items") ;
-console.log("inserting new data") ;
-console.log("merged data") ;
+
